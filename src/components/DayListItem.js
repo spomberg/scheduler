8cc3a68f3f19c3d3cab775083cc5a84c43116a8f
Marcos Spomberg <marcosspomberg@gmail.com>
2022-02-27 15:44:43 -0500

    Request days from API

---
+++ src/components/DayListItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  const formatSpots = (spots) => {
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    }
+
+    if (spots === 1) {
+      return `1 spot remaining`;
+    }
+
+    if (spots < 1) {
+      return `no spots remaining`;
+    }
+  }
+  
+  return (
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
