b43543c6b8395192311e5f865ce6d8063da57a77
Marcos Spomberg <marcosspomberg@gmail.com>
2022-03-03 19:33:54 -0500

    Implement delete appointment feature

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,6 +49,27 @@ export default function Application(props) {
     })
   };
 
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+    return Promise.resolve(axios.delete(`/api/appointments/${id}`))
+    .then(() => {
+      setState({
+        ...state,
+        appointments
+      })
+    })
+    .catch((err) => console.log(err))
+  }
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -83,6 +104,7 @@ export default function Application(props) {
             interview={interview}
             interviewers={interviewers}
             bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
             />
          )
         })}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,6 +11,7 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -30,6 +31,14 @@ export default function Appointment(props) {
     .catch((err) => console.log(err))
   }
 
+  function cancel() {
+    transition(DELETING);
+
+    props.cancelInterview(props.id)
+    .then(() => transition(EMPTY))
+    .catch((err) => console.log(err))
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
@@ -38,6 +47,7 @@ export default function Appointment(props) {
         <Show 
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={cancel}
         />
       )}
       {mode === CREATE && (
@@ -47,7 +57,8 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
-      {mode === SAVING && <Status message={"Saving appointment"}/>}
+      {mode === SAVING && <Status message={"Saving"}/>}
+      {mode === DELETING && <Status message={"Deleting"}/>}
     </article>
   );
 }
